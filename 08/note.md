cad931caa8a8c723a9071f38758ac3ebd15f3cfa
Bob Lied <boblied+github@gmail.com>
2025-12-08 09:31:07 -0600

    AOC 2025 Day 8 done

--- 08/note.md
+++ 08/note.md
@@ -3,6 +3,30 @@
 
 ## Task 1
 
-Here's the quantum leap to the next level, graphs in 3D.
+Every pair of distances between 1000 points is 499,500 combinations.
+Not unreasonable to calculate them all and hold in memory.
+
+- Useful to have a Point class.
+
+- Use a priority queue to process in order by distance. Maybe could have
+just sorted? The values in the queue are pairs of Points; the weight is
+the distance between them.
+
+- Accumulate an array of circuits. Each circuit is a hash table for fast
+lookup of the points that are in it. Make a class around the collection.
+
+- Three possibilities for adding a pair:
+  - Haven't seen either point: push a new circuit onto the array
+  - Both oints exist in two different circuits: merge the 2nd circuit into the first
+  - One point exists in a circuit: add the other point to that circuit
+
 
 ## Task 2
+
+- Set the limit past 499,500 and place every pair into a circuit.
+
+- Stop if the size of the first circuit hits the number of boxes.
+
+- Nastiest data would be to have to get to pair number 499,500 before the
+circuit is complete. That would take about 2 minutes to calculate. Turns
+out that the data is such that it completes much faster than that.
---
+++ 08/task-2.pl
@@ -0,0 +1,135 @@
+#!/usr/bin/env perl
+# vim:set ts=4 sw=4 sts=4 et ai wm=0 nu:
+#=============================================================================
+# Copyright (c) 2025, Bob Lied
+#=============================================================================
+# task-2.pl Advent of Code 2025 Day 8 Part 2  Playground
+#=============================================================================
+
+use v5.42;
+use FindBin qw($Bin); use lib "$FindBin::Bin/../../lib"; use AOC;
+
+my $CircuitCount = 3;
+my $Limit = 1000;
+AOC::setup({"circuit:i" => \$CircuitCount, "limit:i" => \$Limit });
+
+use Feature::Compat::Class;
+use Array::Heap::PriorityQueue::Numeric;
+
+class Point {
+    field $name :param :reader //= "0,0,0";
+    field @p : reader;
+
+    use constant { X => 0, Y => 1, Z => 2 };
+    ADJUST {
+        @p = split(",", $name);
+    }
+
+    method x() { $p[X] }
+    method y() { $p[Y] }
+    method z() { $p[Z] }
+
+    method show() { "($name)" }
+    use overload '""' => sub { $_[0]->show() };
+
+    method dist($other) {
+        use List::Util qw/sum/;
+        return sum map { $_ * $_ }
+            map { ($other->p)[$_] - ($self->p)[$_] } X, Y, Z;
+    }
+}
+
+
+$logger->info("START");
+
+my @Box;
+{
+    chomp(my @input = <>);
+    @Box = map { Point->new(name=>$_) } @input;
+}
+$logger->info(scalar(@Box), " junction boxes");
+$logger->info("first: ", $Box[0]->show, " last: ", $Box[-1]->show);
+
+my $pq = Array::Heap::PriorityQueue::Numeric->new;
+
+for my $first ( 0 .. $#Box-1 )
+{
+    my $box1 = $Box[$first];
+    for my $box2 ( @Box[ $first+1 .. $#Box ] )
+    {
+        my $d = $box1->dist($box2);
+        $pq->add( [$box1,$box2],  $d );
+        $logger->debug("\nBUILD: ", $box1->name," ",$box2->name," ",$d);
+    }
+}
+$logger->info("inserted ", $pq->size(), " into priority queue");
+
+my $Total = 0;
+
+class CircuitList {
+    field $logger :param;
+    field @circuit;
+    ADJUST{ @circuit = ( {} ) }
+
+    method find($box)
+    {
+        for my ($where, $c) ( indexed @circuit )
+        {
+            return $where if exists $c->{$box};
+        }
+        return 0;
+    }
+
+    method add($box1, $box2)
+    {
+        my $where1; my $where2;
+        $where1 = $self->find($box1);
+        $where2 = $self->find($box2);
+
+        if ( $where1 == 0 && $where2 == 0 )
+        {
+            push @circuit, { $box1 => 1 , $box2 => 1 };
+            $logger->debug("New, added $box1 $box2, count=", $#circuit);
+        }
+        elsif ( $where1 && $where2 && $where1 != $where2 )
+        {
+            $logger->debug("Merged, $box1 $box2, now in $where1");
+            $circuit[$where1]{$_} = 1 for keys %{$circuit[$where2]};
+            splice(@circuit, $where2, 1); # $circuit[$where2] = {};
+        }
+        elsif ( $where1 || $where2 )
+        {
+            my $where = ( $where1 != 0 ) ? $where1 : $where2;
+            $circuit[$where]{$box1} = 1;
+            $circuit[$where]{$box2} = 1;
+            $logger->debug("Found, added $box1, $box2 to $where:\n", join(" | ", sort keys %{$circuit[$where]}));
+        }
+    }
+
+    method multiply($howMany)
+    {
+        use List::Util qw/product/;
+        my @n = sort { $b <=> $a } map { scalar(keys %$_) } @circuit;
+        return List::Util::product @n[0..$howMany-1];
+    }
+
+    method size($where) { return scalar keys %{$circuit[$where]} }
+}
+
+my $Circuit = CircuitList->new(logger=>$logger);
+
+while ( $Limit-- > 0  &&  (my $pair = $pq->get()) )
+{
+    $Circuit->add($pair->[0]->name, $pair->[1]->name);
+    if ( $Circuit->size(1) == @Box )
+    {
+        $logger->info("Last pair: $pair->@*");
+        $Total = $pair->[0]->x * $pair->[1]->x;
+        last;
+    }
+}
+
+say $Total;
+
+
+$logger->info("FINISH");
